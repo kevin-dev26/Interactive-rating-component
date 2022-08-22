dfe0a6348482e5f1667c296a19507163e0d23e9c
K Campos <mktdigitalkevin@gmail.com>
2022-08-22 12:03:45 -0300

    .

--- javascript/script.js
+++ javascript/script.js
@@ -4,23 +4,23 @@ const cartaoAgradecimento = document.querySelector('.cartao-agradecimento')
 const cartaoPesquisa = document.querySelector('.cartao-pesquisa')
 const notasDaPesquisa = document.querySelectorAll('.nota')
 const botaoSubmit = document.querySelector('.submit-button')
-
+let notaEscolhidaFinal = 4 
 
 /* 
 Clicar na nota e pintar o fundo da nota com cinza mais claro, dar o classe, nota-escolhida para aquela nota e remover a classe de quem estiver com ela. 
 */
-
-botaoSubmit.addEventListener('click', () => {
-    cartaoPesquisa.classList.add('hide')
-    cartaoAgradecimento.classList.remove('hide')
-    cartaoPesquisa.classList.remove('ativo')
-})
 
 
 /* 
 Ao clicar no botão de submit, esconde o cartão de pesquisa,
 aparece o cartão de agradecimento.
  */
+botaoSubmit.addEventListener('click', () => {
+    cartaoPesquisa.classList.add('hide')
+    cartaoAgradecimento.classList.remove('hide')
+    cartaoPesquisa.classList.remove('ativo')
+})
+
 
 
 
